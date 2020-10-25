57605e9c3c3a1c0f9fc0e084f9652533fe19caab
nsankuratri <narayana.sankuratri@gmail.com>
2020-10-25 21:25:12 +0000

    1st update

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -8,6 +8,8 @@ public class App
 {
     public static void main( String[] args )
     {
+
         System.out.println( "Hello World!first test" );
+        System.out.println( "Hello World!first devtest branch created" );
     }
 }
